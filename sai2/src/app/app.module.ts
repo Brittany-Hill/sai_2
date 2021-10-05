35d265e4d24e2d632adf57037cc0d7925459e9e2
Brittany-Hill <bh41@students.uwf.edu>
2021-10-05 18:23:21 -0500

    adding firebase config

--- sai2/src/app/app.module.ts
+++ sai2/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { AngularFireAuthModule } from "@angular/fire/compat/auth";
+import { AngularFireModule } from '@angular/fire/compat';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AppRoutingModule,
+    AngularFireAuthModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig) // Your config
   ],
   providers: [],
   bootstrap: [AppComponent]
