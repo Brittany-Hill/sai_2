d3691b970233514fb88f463996bb82dd7d2739a6
Tyler Blackmon <tsb34@students.uwf.edu>
2021-10-15 15:16:28 -0500

    didsomethings

--- sai2/src/app/app.module.ts
+++ sai2/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolbarComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
