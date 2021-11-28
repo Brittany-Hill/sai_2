4ec786b3b550494f4fb0b23ee0f5a3da0f87c0eb
Tyler Blackmon <tsb34@students.uwf.edu>
2021-11-28 15:22:54 -0600

    Step in right direction

--- sai2/src/app/teacher-sai-view/teacher-sai-view.component.ts
+++ sai2/src/app/teacher-sai-view/teacher-sai-view.component.ts
@@ -13,34 +13,21 @@ export class TeacherSaiViewComponent implements OnInit {
   courseSAI:Course = new Course("Discrete Structures",0,0);
 
   public answers: string[] = [];
-  public questions: string[] = [];
-  private something: any;
   public numberOfStudents: number = 0
   public courseName = 'Discrete Structures'
-  private classID = 'cen 3031';
+  private classID = 'cen3031';
 
   constructor(private facilityQuestionsService: FacilityService, private saiQuestionsService: SaiQuestionsService) { 
-    facilityQuestionsService.getAllQuestions().forEach(value => {
-      if(this.numberOfStudents === 0){
-        this.numberOfStudents = value.length;
-      }
-      value.forEach(entry => {
-        this.answers.push(entry.question1);
-        this.answers.push(entry.question2);
-        this.answers.push(entry.question3);
-        this.answers.push(entry.question4);
-        this.answers.push(entry.question5);
-      })
-    })
-
     saiQuestionsService.getAllQuestions().forEach(value => {
       value.forEach(set => {
         if(set.classID.toString() === this.classID){
-          this.questions.push(set.question1);
-          this.questions.push(set.question2);
-          this.questions.push(set.question3);
-          this.questions.push(set.question4);
-          this.questions.push(set.question5);
+          this.answers.push(set.question1);
+          this.answers.push(set.question2);
+          this.answers.push(set.question3);
+          this.answers.push(set.question4);
+          this.answers.push(set.question5);
+          this.answers.push(set.question6);
+          this.answers.push(set.question7);
         }
       })
     })
